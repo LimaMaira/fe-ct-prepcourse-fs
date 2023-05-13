c981b3c7af3f3e547f70c6865e0946f198054fa1
MairaLima <mairaailenlima@gmail.com>
2023-05-13 00:08:20 -0300

    commit_solution

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,71 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if (x === y) {
+      res = true;
+   } else {
+      res = false;
+   }
+   return res;
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
-   // Tu código:
+   // Tu código: 
+   if (str1.length === str2.length){
+      res = true;
+   } else {
+      res = false;
+   }
+   return res;
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if ( num < 90 ) {
+      res = true
+   } else {
+      res = false
+   }
+   return res;
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num > 50 ){
+      return true
+   } else {
+      return false
+   }
 }
+
+//console.log(mayorQueCincuenta(48));
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 === 0) {
+      return true
+      } else {
+      return false
+      }
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 == '1') {
+      return true 
+   } else {
+      return false 
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
